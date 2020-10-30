a45efca9ac9877865758c64d53f7376bcc6a0845
Vladimir Karashchuk <43484800+OffworldDemon@users.noreply.github.com>
2020-10-29 21:21:16 -0400

    Pancake added

---
+++ Assets/ObstacleRandomizer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleRandomizer : MonoBehaviour
+{
+    int obstacleGenerator;
+    GameObject[] obstacles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obstacleGenerator = Random.Range(0, 2);
+        GameObject newObstacle;
+        switch(obstacleGenerator)
+        {
+            default:
+                break;
+            case 0:
+                break;
+            case 1:
+                //newCreation = Instantiate(wall1, new Vector3(0, 0, transform.position.z - 6), Quaternion.identity);
+                //newCreation.transform.SetParent(world.transform);
+                break;
+        }
+
+
+    }
+}
--- Assets/WorldMover.cs
+++ Assets/WorldMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WorldMover : MonoBehaviour
 {
 
-    public float moveSpeed = 0.05f;
+    public float moveSpeed = 0.1f;
     public float score = 0f;
 
     // Start is called before the first frame update
@@ -17,9 +17,10 @@ public class WorldMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        score = transform.position.z * 10;
-        moveSpeed += 0.01f * Time.deltaTime;
+        //score = transform.position.z * 10;
+        //moveSpeed += 0.01f * Time.deltaTime;
         transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+        Time.timeScale += 0.005f * Time.deltaTime;
     }
 
 
