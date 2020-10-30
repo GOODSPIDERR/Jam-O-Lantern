5f46c56de5ad7201963907214655209d08a2dbd2
Vladimir Karashchuk <43484800+OffworldDemon@users.noreply.github.com>
2020-10-30 18:55:47 -0400

    Score counter

---
+++ Assets/ScoreTracker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour
+{
+
+    Text text;
+    GameObject world;
+    WorldMover mover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        world = GameObject.Find("World");
+        mover = world.GetComponent<WorldMover>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = mover.score.ToString();
+    }
+}
